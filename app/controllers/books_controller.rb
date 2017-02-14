8388520558f2f863f56aa51ee3d14f323066e158
Justin <justintenser@me.com>
2017-02-14 12:34:57 -0500

    done

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,20 @@
 class BooksController < ApplicationController
 
+def index
+    @books = Book.all
+
+    respond_to do |format|
+    format.html
+    format.text
+    format.csv do
+    render text: Book.generate_csv(@books)
+    end
+    format.json do
+           render json: @books
+         end
+
+  end
+
+end
 
 end
